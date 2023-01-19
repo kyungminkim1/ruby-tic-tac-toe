b1eb959c632eb426a8f17a5f45d7764928336721
kyungminkim1 <kyungminkimnz@gmail.com>
2023-01-19 21:39:29 +1300

    Outline basic game architecture

---
+++ tic_tac_toe.rb
@@ -0,0 +1,29 @@
+# Recreate tic-tac-toe using Ruby
+# Things to consider:
+# - classes:
+# -- Player
+# -- Board
+# -- Piece
+# -- Cross (inherit from Piece)
+# -- Circle (inherit from Piece)
+
+# - instance variables:
+# -- Player:
+# --- @piece_type = Piece.new
+# -- Board:
+# --- @@board_space
+# -- Piece:
+# --- @symbol
+
+
+# - methods:
+# Player
+# -- place_piece
+# Board:
+# -- declare_result
+# -- wait_for_turn
+# -- check_rows
+# -- check_columns
+# -- check_diagonals
+# -- print_board
+# -- get_player_input
