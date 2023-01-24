b72419a9e58120e3de2927aafb3ec8af39efff9b
kyungminkim1 <kyungminkimnz@gmail.com>
2023-01-24 22:17:19 +1300

    Fix matching? methods and announce_victory Previously comparisions was made with the Piece object itself instead of the Piece's symbol

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -48,7 +48,7 @@ class Board
 
   # call after player finishes their move
   def check_board(player)
-    if matching_row?(player) || matching_column?(player) || matching_diagonal?(player)
+    if matching_row?(player.piece) || matching_column?(player.piece) || matching_diagonal?(player.piece)
       # announce victory for player 1/2
       announce_victory(player)
     elsif board_full?
@@ -65,42 +65,42 @@ class Board
     @board_space[row_index][column_index] == ' '
   end
 
-  def matching_row?(player)
+  def matching_row?(piece)
     # check top row
-    if @board_space[0][0] == player.symbol && @board_space[0][1] == player.symbol && @board_space[0][2] == player.symbol
+    if @board_space[0][0] == piece.symbol && @board_space[0][1] == piece.symbol && @board_space[0][2] == piece.symbol
       return true
     # check middle row
-    elsif @board_space[1][0] == player.symbol && @board_space[1][1] == player.symbol && @board_space[1][2] == player.symbol
+    elsif @board_space[1][0] == piece.symbol && @board_space[1][1] == piece.symbol && @board_space[1][2] == piece.symbol
       return true
     # check bottom row
-    elsif @board_space[2][0] == player.symbol && @board_space[2][1] == player.symbol && @board_space[2][2] == player.symbol
+    elsif @board_space[2][0] == piece.symbol && @board_space[2][1] == piece.symbol && @board_space[2][2] == piece.symbol
       return true
     end
 
     false
   end
 
-  def matching_column?(player)
+  def matching_column?(piece)
     # check left column
-    if @board_space[0][0] == player.symbol && @board_space[1][0] == player.symbol && @board_space[2][0] == player.symbol
+    if @board_space[0][0] == piece.symbol && @board_space[1][0] == piece.symbol && @board_space[2][0] == piece.symbol
       return true
     # check middle column
-    elsif @board_space[0][1] == player.symbol && @board_space[1][1] == player.symbol && @board_space[2][1] == player.symbol
+    elsif @board_space[0][1] == piece.symbol && @board_space[1][1] == piece.symbol && @board_space[2][1] == piece.symbol
       return true
     # check right column
-    elsif @board_space[0][2] == player.symbol && @board_space[1][2] == player.symbol && @board_space[2][2] == player.symbol
+    elsif @board_space[0][2] == piece.symbol && @board_space[1][2] == piece.symbol && @board_space[2][2] == piece.symbol
       return true
     end
 
     false
   end
 
-  def matching_diagonal?(player)
+  def matching_diagonal?(piece)
     # check top left-to-bottom right diagonal
-    if @board_space[0][0] == player.symbol && @board_space[1][1] == player.symbol && @board_space[2][2] == player.symbol
+    if @board_space[0][0] == piece.symbol && @board_space[1][1] == piece.symbol && @board_space[2][2] == piece.symbol
       return true
     # check top right-to-bottom left diagonal
-    elsif @board_space[0][2] == player.symbol && @board_space[1][1] == player.symbol && @board_space[2][0] == player.symbol
+    elsif @board_space[0][2] == piece.symbol && @board_space[1][1] == piece.symbol && @board_space[2][0] == piece.symbol
       return true
     end
     
@@ -109,14 +109,19 @@ class Board
 
   def announce_victory(player)
     puts "#{player.name} wins!"
-    puts `"""""""""""""""`
+    puts '"""""""""""""""'
     puts print_board
-    puts `"""""""""""""""` 
+    puts '"""""""""""""""'
+  end
+
+  def board_full?
+
   end
 
   def announce_draw
 
   end
+  
 
 end
 
@@ -150,7 +155,10 @@ end
 
 b = Board.new
 b.print_board
-player_one = Player.new('x')
+player_one = Player.new('Player 1', 'x')
 b.add_piece(player_one.piece, 0, 0)
 b.print_board
 b.add_piece(player_one.piece, 0, 0)
+b.add_piece(player_one.piece, 1, 0)
+b.add_piece(player_one.piece, 2, 0)
+b.check_board(player_one)
